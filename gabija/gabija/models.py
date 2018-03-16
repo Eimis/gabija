1472944d80d46fd2c7e9e17de728e9d4dfce0429
Eimantas <eimantas.stonys@gmail.com>
2018-03-16 17:03:19 +0100

    Add new 'purchased' field to ShoppingItem model

--- gabija/gabija/models.py
+++ gabija/gabija/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class ShoppingItem(models.Model):
     name = models.CharField(max_length=20)
     added_on = models.DateTimeField(auto_now_add=True)
+    # is the item already purchased?:
+    purchased = models.BooleanField(default=False)
