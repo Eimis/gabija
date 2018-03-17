a6a6ce678d9a4801f4e7b027dc24044237d086a0
Eimantas <eimantas.stonys@gmail.com>
2018-03-17 14:07:00 +0100

    Proper model instance representation

--- gabija/gabija/models.py
+++ gabija/gabija/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class ShoppingItem(models.Model):
     name = models.CharField(max_length=20)
     added_on = models.DateTimeField(auto_now_add=True)
     # is the item already purchased?:
     purchased = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
